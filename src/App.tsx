ce80afa3c8174d68f88979cabd60b3d8ba65a9b5
Philemon Forson <firstkoolaid21@gmail.com>
2023-07-11 11:27:27 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import CandleStickChart from "./components/CandleStickChart";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>My CandleStick Chart</h1>
+        <CandleStickChart />
+      </div>
+    </>
+  );
+}
+
+export default App;
